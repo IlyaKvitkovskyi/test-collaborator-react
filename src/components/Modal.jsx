f76b98d74365792593ed22d9508b895afbc9a17c
Illia Kvitkovskyi <kvitkovskii97@gmail.com>
2022-07-10 11:49:46 +0300

    added styles for modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import './Modal.scss'
+import './Modal.scss';
 
-export default function Modal ({active, setActive}) {
+export default function Modal({ active, setActive }) {
   return (
-    <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
-      <div className={active ? "modal__content active" : "modal__content"} onClick={(e) => e.stopPropagation()}>
-        asd
+    <div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
+      <div
+        className={active ? 'modal__content active' : 'modal__content'}
+        onClick={(e) => e.stopPropagation()}>
+        <span className="modal__content-text">Вы уверены?</span>
+        <div className="modal__content-buttons">
+          <button className="modal__content-btn">Да</button>
+          <button className="modal__content-btn">Нет</button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
